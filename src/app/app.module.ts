a4909b47d08fafac53fbc21b3637dcdcdab5f04e
ozihler <ozihler@gmail.com>
2020-01-25 20:18:25 +0100

    add all stuff i forgot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { WorkoutSelectionComponent } from './workout-selection/workout-selection
 import { WorkoutsOverview } from './workouts-overview/workouts-overview.component';
 import { WorkoutOverview } from './workout-overview/workout-overview.component';
 import { CreateMuscleGroupComponent } from './create-muscle-group/create-muscle-group.component';
-import { WorkoutDetailsView } from './muscle-group-display/workout-details-view.component';
+import { WorkoutDetailsView } from './workout-details-view/workout-details-view.component';
 import { MuscleGroupSelection } from './muscle-group-selection/muscle-group-selection.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { CreateExerciseComponent } from './create-exercise/create-exercise.component';
 import { AddExerciseComponent } from './add-exercise/add-exercise.component';
 import { CreateSetComponent } from './create-set/create-set.component';
 import { FitScreenPipe } from './shared/fit-screen.pipe';
+import { ButtonGroupComponent } from './workout-details-view/button-group/button-group.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { FitScreenPipe } from './shared/fit-screen.pipe';
     CreateExerciseComponent,
     AddExerciseComponent,
     CreateSetComponent,
-    FitScreenPipe
+    FitScreenPipe,
+    ButtonGroupComponent
   ],
   imports: [
     BrowserModule,
