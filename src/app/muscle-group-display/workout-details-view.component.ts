1088dde2423a214f09181ca698913c202bae2c85
ozihler <ozihler@gmail.com>
2020-01-25 19:02:11 +0100

    add toggles

--- src/app/muscle-group-display/workout-details-view.component.ts
+++ src/app/muscle-group-display/workout-details-view.component.ts
@@ -5,18 +5,22 @@ import {MuscleGroup} from "../shared/muscle-group";
   selector: 'app-workout-details-view',
   template: `
     <div *ngFor="let muscleGroup of selectedMuscleGroups">
-      <button>{{muscleGroup.name}}</button>
+      <button (click)="toggleExercises(muscleGroup.name)">{{muscleGroup.name}}<span
+        *ngIf="!shouldShowExercisesOf(muscleGroup)"> ({{muscleGroup.exercises.length}})</span></button>
+
       <div *ngFor="let exercise of muscleGroup.exercises">
-        <button>{{exercise.name}}</button>
-        <div *ngFor="let set of exercise.sets">
-          <span>{{set.repetitions}}</span>
-          <span> [reps]</span>
-          <span> | </span>
-          <span>{{set.weight}}</span>
-          <span> [kg]</span>
-          <span> | </span>
-          <span>{{set.waitingTime}}</span>
-          <span> [s]</span>
+        <div *ngIf="shouldShowExercisesOf(muscleGroup)">
+          <button>{{exercise.name}}</button>
+          <div *ngFor="let set of exercise.sets">
+            <span>{{set.repetitions}}</span>
+            <span> [reps]</span>
+            <span> | </span>
+            <span>{{set.weight}}</span>
+            <span> [kg]</span>
+            <span> | </span>
+            <span>{{set.waitingTime}}</span>
+            <span> [s]</span>
+          </div>
         </div>
       </div>
     </div>
@@ -26,11 +30,32 @@ import {MuscleGroup} from "../shared/muscle-group";
 export class WorkoutDetailsView implements OnInit {
 
   @Input() selectedMuscleGroups: MuscleGroup[] = [];
+  private toggles = [];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.initialiseToggles();
+  }
+
+  private initialiseToggles() {
+    this.selectedMuscleGroups.forEach(muscleGroup => {
+        this.toggles[muscleGroup.name] = false;
+
+        muscleGroup.exercises.forEach(exercise => {
+          this.toggles[exercise.name] = false;
+        })
+      }
+    )
+  }
+
+  toggleExercises(muscleGroupName: string) {
+    this.toggles[muscleGroupName] = !this.toggles[muscleGroupName];
+  }
+
+  shouldShowExercisesOf(muscleGroup: MuscleGroup) {
+    return this.toggles[muscleGroup.name];
   }
 
 }
